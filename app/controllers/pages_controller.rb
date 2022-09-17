95954c7f271226452c7668d9f16f23198b58291e
alexclapou <alex.clapou@yahoo.com>
2022-09-17 13:05:58 +0300

    navigation bar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def profile
+  end
+
+  def dashboard
+  end
+
+  def search
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  get 'dashboard', to: 'pages#dashboard'
+  get 'search', to: 'pages#search'
+  get 'profile', to: 'pages#profile'
 end
