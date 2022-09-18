4f8c63f3e706c87cbdf5a6967b0838d9ea38c281
alexclapou <alex.clapou@yahoo.com>
2022-09-18 22:48:28 +0300

    dark mode

--- app/assets/javascript/custom/theme.js
+++ app/assets/javascript/custom/theme.js
@@ -23,11 +23,11 @@ if(getCookie('theme') == ''){
 // user can choose his theme
 $(document).on('click', '.choose-theme', function() {
     var radioValue = $("input[name='theme-radio']:checked").val();
-    document.cookie = `theme=${radioValue}`
+    console.log(radioValue)
     document.body.classList.remove("dark");
     document.body.classList.remove("auto");
     document.body.classList.remove("light");
+    document.cookie = `theme=${radioValue}`
     if(radioValue != 'auto')
         document.body.classList.toggle(radioValue);
-    console.log(radioValue)
 });
