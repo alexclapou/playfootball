210117f96b30044989e510df4fda3970b38c6a37
alexclapou <alex.clapou@yahoo.com>
2022-09-19 17:52:02 +0300

    lang&theme

--- app/assets/javascript/custom/theme.js
+++ app/assets/javascript/custom/theme.js
@@ -1,33 +1,18 @@
-function getCookie(cname) {
-  let name = cname + "=";
-  let decodedCookie = decodeURIComponent(document.cookie);
-  let ca = decodedCookie.split(';');
-  for(let i = 0; i <ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
-}
+import { getCookie } from "./cookies";
 
 // when user first visit the application keep the default mode
 if(getCookie('theme') == ''){
-    document.cookie = 'theme=auto'
+    document.cookie = 'theme=auto;path=/'
 }
-
 
 // user can choose his theme
 $(document).on('click', '.choose-theme', function() {
     var radioValue = $("input[name='theme-radio']:checked").val();
-    console.log(radioValue)
     document.body.classList.remove("dark");
     document.body.classList.remove("auto");
     document.body.classList.remove("light");
-    document.cookie = `theme=${radioValue}`
+    document.cookie = `theme=${radioValue};path=/`
+    
     if(radioValue != 'auto')
         document.body.classList.toggle(radioValue);
 });
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    devise_for :users
 
-  root 'pages#home'
-  get 'dashboard', to: 'pages#dashboard'
-  get 'search', to: 'pages#search'
-  # get 'profile', to: 'pages#profile'
+    root 'pages#home'
+    get 'dashboard', to: 'pages#dashboard'
+    get 'search', to: 'pages#search'
+    # get 'profile', to: 'pages#profile'
 
-  get 'profile', to: 'users#show', as: :profile
-  resources :users, only: %i[show]
+    get 'profile', to: 'users#show', as: :profile
+    resources :users, only: %i[show]
+  end
 end
