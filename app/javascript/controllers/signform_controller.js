38829c06a0155a7bb76e4b82cdbf0fae00b00a36
alexclapou <alex.clapou@yahoo.com>
2022-10-01 23:32:00 +0300

    sign in validation

--- app/javascript/controllers/signform_controller.js
+++ app/javascript/controllers/signform_controller.js
@@ -2,25 +2,41 @@ import { Controller } from "@hotwired/stimulus"
 import { validEmail } from "helpers/email_helper"
 
 export default class extends Controller {
-  static targets = ['email', 'password', 'button']
+  static targets = ['email', 'emailInput', 'password', 'passwordInput', 'button', 'warning']
 
   connect() {
     this.isValidCombination()
+    this.wrongCredentials()
   }
-  
+
   isValidEmail(){
-    if(validEmail(this.emailTarget.value.toLowerCase()))
+    if(validEmail(this.emailTarget.value.toLowerCase())){
+      this.emailTarget.classList.remove('warning-input')
+      this.emailInputTarget.classList.remove('warning-label')
       return true
+    }
     return false
   }
 
   isValidPassword(){
-    if(this.passwordTarget.value)
+    if(this.passwordTarget.value){
+      this.passwordTarget.classList.remove('warning-input')
+      this.passwordInputTarget.classList.remove('warning-label')
       return true
+    }
     return false
   }
-  cons(event){
-    console.log('sex')
+
+  wrongCredentials(){
+    if(document.getElementById('warningId'))
+      this.markInvalidCredentials()
+  }
+  
+  markInvalidCredentials(){
+    this.emailTarget.classList.add('warning-input')
+    this.emailInputTarget.classList.add('warning-label')
+    this.passwordTarget.classList.add('warning-input')
+    this.passwordInputTarget.classList.add('warning-label')
   }
 
   isValidCombination(){
@@ -28,9 +44,5 @@ export default class extends Controller {
       this.buttonTarget.disabled = false
     else
       this.buttonTarget.setAttribute('disabled', 'disabled')
-  }
-
-  sbmt(){
-    console.log('s')
   }
 }
